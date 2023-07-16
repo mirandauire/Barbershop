6a40f6ac36408fa78b7cbc76e23cd099e706ecb5
miranduire <138498451+mirandauire@users.noreply.github.com>
2023-07-16 14:50:57 -0300

    #Revert

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,32 +12,49 @@
     <link rel="stylesheet" href="assets/style.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
-
 <body>
-<?php
-require_once 'menu.php';
-?>
-    
+    <a class="logo" href="#"><img class="img_logo" src="assets/images/logo.svg"></img></a>
+    <div id="fake-navbar"></div>
+    <div class="navbar">
+        <ul>
+            <li><a href="#section0">home</a></li>
+            <li><a href="#section1">sobre nós</a></li>
+            <li><a href="#section3">serviços</a></li>
+            <li><a href="#section5">contato</a></li>
+            <li><a href="#section7">agendamento</a></li>
+        </ul>
+        <span class="navbar-rtext">Aberto de Segunda a Sábado, das 9h às 19h</span>
+        <a class="instagram_icon" href="https://www.instagram.com/donfelixbarbershop/"><i class="bi bi-instagram"></i></a>
+        <div id="min-navbar">
+            <ul>
+                <li><a class = home href="#section0">home</a></li>
+                <li><a href="#section1">sobre nós</a></li>
+                <li><a href="#section3">serviços</a></li>
+                <li><a href="#section5">contato</a></li>
+                <li><a href="#section7">agendamento</a></li>
+            </ul>
+        </div>
+    </div>
     <div class="sections">
         <div id="section0">
             <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="assets/images/alternative/4.jpg" class="d-block w-100" alt="images/alternative/2.jpg">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="assets/images/alternative/3.jpg" class="d-block w-100" alt="images/alternative/3.jpg">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="assets/images/alternative/5.jpg" class="d-block w-100" alt="images/alternative/1.jpg">
-                    </div>
-                </div>
-            </div>
+                  <div class="carousel-item active">
+                    <img src="assets/images/alternative/4.jpg" class="d-block w-100" alt="images/alternative/2.jpg">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="assets/images/alternative/3.jpg" class="d-block w-100" alt="images/alternative/3.jpg">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="assets/images/alternative/5.jpg" class="d-block w-100" alt="images/alternative/1.jpg">
+                  </div>
+                </div>             
+              </div>
         </div>
         <div id="section1">
             <h2>Sobre nós</h2>
-            <p>A Don Felix é a barbearia da sua época. Focada em excelência, ela conta com tudo que o homem moderno precisa. Um ambiente confortável, descontraído e exclusivamente masculino, com cuidados para todos os estilos de barba e cabelo, além de serviços como dia do noivo. Uma barbearia premium, para cuidar do visual, tomar uma cerveja gelada ou assistir aos seus esportes favoritos.
-                Seja um de nossos clientes exclusivos.
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci dolor vero doloremque maiores exercitationem est rerum iste delectus ab incidunt fugit velit voluptates inventore similique, totam, aliquam non quibusdam. Et.
+                Accusamus, possimus eaque sequi error explicabo harum rem asperiores suscipit odio qui sit doloribus eveniet debitis eum autem itaque! Autem, ab architecto nemo quas asperiores maiores eaque nostrum velit praesentium.
             </p>
             <div class="img_section"></div>
         </div>
@@ -56,7 +72,7 @@ require_once 'menu.php';
             <div class="background"></div>
             <img class="star-rating" src="assets/images/icons/star-rating.svg"></img>
             <text>“Primeira vez na barbearia e saí plenamente satisfeito. Além do resultado fantástico de cabelo e barba, o atendimento do Reny foi espetacular. Virei freguês. Continuo freguês”
-            </text>
+            </text>            
         </div>
         <div id="section5">
             <h2>Contato</h2>
@@ -66,7 +82,7 @@ require_once 'menu.php';
         </div>
         <div id="section6">
             <div class="background"></div>
-            <text>"Barba, cabelo e conforto!"</text>
+            <text>"Barba, cabelo e conforto!"</text>            
         </div>
         <div id="section7">
             <h2>Agendamento</h2>
@@ -74,10 +90,10 @@ require_once 'menu.php';
                 Accusamus, possimus eaque sequi error explicabo harum rem asperiores suscipit odio qui sit doloribus eveniet debitis eum autem itaque! Autem, ab architecto nemo quas asperiores maiores eaque nostrum velit praesentium.
             </p>
         </div>
-    <?php
-    require_once 'rodape.php';
-    ?>
+        <div id="section8">
+            <div class="background"></div>
+            <text>&copy; 2023 Copyright &reg; Todos direitos reservados</text>
+        </div>
     </div>
 </body>
-
 </html>
